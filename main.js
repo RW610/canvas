86a9f77c243fcce9af7f03501049d9ada9fb5c6f
RW610 <118364581+RW610@users.noreply.github.com>
2022-11-30 22:49:32 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,41 @@
 let canvas 
 let ctx
-let width = canvas.width
-let height = canvas.height
+let width
+let height
 let points = []
 let numPoints = 5
 let hue = Math.random() * 360
 let color = 'hsla(' + (Math.random() * 360) + ', 50%, 50%, 1)'
+canvas = document.getElementById('canvas')
+ctx = canvas.getContext('2d')
+width = canvas.width
+height = canvas.height
 
-function init(){
-	canvas = document.getElementById('canvas')
-	ctx = canvas.getContext('2d')
-    width = canvas.width
-    height = canvas.height
-	//center_x = stage.canvas.width/2;
-	//center_y = stage.canvas.height/2-100;
-	//ImageRandom(stage,center_x,center_y);
-	//stage.update();
+let rand = function (x) {
+      return Math.random() * x
+    }
+
+    let point = function () {
+      this.x = rand(width)
+      this.y = rand(height)
+      this.xSpeed = rand(1) - .5
+      this.ySpeed = rand(1) - .5
+    }
+
+    point.prototype.move = function () {
+      this.x += this.xSpeed
+      this.y += this.ySpeed
+    }
+
+    point.prototype.checkCollision = function () {
+      if (this.x < 0 || this.x > width) {
+        this.xSpeed = -this.xSpeed
+      }
+      if (this.y < 0 || this.y > height) {
+        this.ySpeed = -this.ySpeed
+      }
+    }
+
     for (let i = 0; i < numPoints; i++) {
       points[i] = new point()
     }
@@ -41,31 +61,3 @@ function init(){
       ctx.stroke()
       //ctx.strokeRect(0, 0, width, height)
     }, 1)
-}
-    
-    let rand = function (x) {
-      return Math.random() * x
-    }
-
-    let point = function () {
-      this.x = rand(width)
-      this.y = rand(height)
-      this.xSpeed = rand(1) - .5
-      this.ySpeed = rand(1) - .5
-    }
-
-    point.prototype.move = function () {
-      this.x += this.xSpeed
-      this.y += this.ySpeed
-    }
-
-    point.prototype.checkCollision = function () {
-      if (this.x < 0 || this.x > width) {
-        this.xSpeed = -this.xSpeed
-      }
-      if (this.y < 0 || this.y > height) {
-        this.ySpeed = -this.ySpeed
-      }
-    }
-
-    
